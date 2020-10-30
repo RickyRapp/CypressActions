a7c3f02c65fe3e073b407db15be37510a360903d
Jure Perak <jperak@mono-software.com>
2020-10-30 09:53:13 +0100

    Adding meta tags through helmet package

--- src/themes/default/application/public/pages/Daf101Template.jsx
+++ src/themes/default/application/public/pages/Daf101Template.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { defaultTemplate } from 'core/hoc';
+import { Helmet } from 'react-helmet';
 
 function Daf101Template() {
 
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>DAF - The Donors' Fund</title>
+            </Helmet>
             DAF 101
         </div>
     );
--- src/themes/default/application/public/pages/WhatWeOfferTemplate.jsx
+++ src/themes/default/application/public/pages/WhatWeOfferTemplate.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { defaultTemplate } from 'core/hoc';
+import { Helmet } from 'react-helmet';
 
 function WhatWeOfferTemplate() {
 
     return (
         <div>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>What We Offer - The Donors' Fund</title>
+            </Helmet>
             What We Offer
         </div>
     );
