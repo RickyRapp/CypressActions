6ba59da08bd201ba145332e4c90e966d99c2d798
arajher <arajher@mono-software.com>
2020-12-08 13:34:21 +0100

    style scanner step 1 and step 2 and style stepper

--- src/themes/default/application/session/components/Step2Template.jsx
+++ src/themes/default/application/session/components/Step2Template.jsx
@@ -1,65 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
-import {
-    BasicInput,
-    BaasicButton,
-    BaasicFieldDropdown,
-    NumberFormatInputField
-} from 'core/components';
+import { BasicInput, BaasicButton, BaasicFieldDropdown, NumberFormatInputField } from 'core/components';
 
-function Step2Template({
-    form,
-    onPreviousStepClick,
-    onNextStepClick,
-    charityDropdownStore
-}) {
-    return (
-        <React.Fragment>
-            <div className="card--primary card--med card--session card--session--start">
-                <h3 className="type--lrg type--wgt--medium u-mar--bottom--med u-mar--left--sml">General Data</h3>
-                <div className="row">
-                    <div className="col col-sml-12 u-mar--bottom--med">
-                        <div className="col col-sml-12 u-mar--bottom--sml">
-                            <BasicInput field={form.$('fullName')} />
-                        </div>
-                        <div className="col col-sml-12 u-mar--bottom--sml">
-                            <NumberFormatInputField field={form.$('phoneNumber')} />
-                        </div>
-                        <div className="col col-sml-12 u-mar--bottom--sml">
-                            <BasicInput field={form.$('email')} />
-                        </div>
-                        <div className="col col-sml-12 u-mar--bottom--sml">
-                            <BaasicFieldDropdown field={form.$('charityId')} store={charityDropdownStore} />
-                        </div>
-                        <div className="col col-sml-12 u-mar--bottom--med">
-                            <BasicInput field={form.$('description')} />
-                        </div>
-                        <div className="col col-sml-12">
-                            <BaasicButton
-                                className="btn btn--base btn--ghost u-mar--right--sml"
-                                onClick={onPreviousStepClick}
-                                label='SESSION.CREATE.STEP2.BUTTONS.BACK'
-                            />
-                            <BaasicButton
-                                className="btn btn--base btn--primary"
-                                onClick={onNextStepClick}
-                                label='SESSION.CREATE.STEP2.BUTTONS.SAVE'
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    )
+function Step2Template({ form, onPreviousStepClick, onNextStepClick, charityDropdownStore }) {
+	return (
+		<React.Fragment>
+			<div className="scanner card--med">
+				{/* <h3 className="type--lrg type--wgt--medium u-mar--bottom--med u-mar--left--sml">General Data</h3> */}
+				<div className="row">
+					<div className="col col-sml-12 u-mar--bottom--med">
+						<div className="col col-sml-12 u-mar--bottom--lrg">
+							<BasicInput field={form.$('fullName')} />
+						</div>
+						<div className="col col-sml-12 u-mar--bottom--lrg">
+							<NumberFormatInputField field={form.$('phoneNumber')} />
+						</div>
+						<div className="col col-sml-12 u-mar--bottom--lrg">
+							<BaasicFieldDropdown field={form.$('charityId')} store={charityDropdownStore} />
+						</div>
+						<div className="col col-sml-12 u-mar--bottom--lrg">
+							<BasicInput field={form.$('description')} />
+						</div>
+						<div className="col col-sml-12 u-mar--bottom--lrg">
+							<p className="type--med type--wgt--regular type--color--opaque u-mar--bottom--med">
+								To receive a report of this session please enter your email address bellow:
+							</p>
+							<BasicInput field={form.$('email')} />
+						</div>
+						<div className="col col-sml-12">
+							<div className="scanner__footer">
+								<BaasicButton
+									className="btn btn--med btn--med--100 btn--primary u-mar--right--sml"
+									onClick={onPreviousStepClick}
+									label="SESSION.CREATE.STEP2.BUTTONS.BACK"
+								/>
+								<BaasicButton
+									className="btn btn--med btn--med--100 btn--secondary"
+									onClick={onNextStepClick}
+									label="SESSION.CREATE.STEP2.BUTTONS.SAVE"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
 
 Step2Template.propTypes = {
-    form: PropTypes.object.isRequired,
-    onPreviousStepClick: PropTypes.func.isRequired,
-    onNextStepClick: PropTypes.func.isRequired,
-    charityDropdownStore: PropTypes.object.isRequired,
-    t: PropTypes.func
+	form: PropTypes.object.isRequired,
+	onPreviousStepClick: PropTypes.func.isRequired,
+	onNextStepClick: PropTypes.func.isRequired,
+	charityDropdownStore: PropTypes.object.isRequired,
+	t: PropTypes.func,
 };
 
 export default defaultTemplate(Step2Template);
--- src/themes/default/application/session/components/StepCounter.jsx
+++ src/themes/default/application/session/components/StepCounter.jsx
@@ -3,31 +3,43 @@ import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
 import _ from 'lodash';
 
-const StepCounter = function ({ currentStep, steps }) {
-    return (
-        <div className="card--primary card--med type--center">
-            <span className="type--base type--wgt--medium">
-                {_.map(steps, step => {
-                    return <Step
-                        key={step}
-                        step={step}
-                        selected={step === currentStep}
-                    />
-                })}
-            </span>
-        </div>)
-}
-
-StepCounter.propTypes = {
-    currentStep: PropTypes.number.isRequired,
-    steps: PropTypes.array.isRequired
+const StepCounter = function({ currentStep, steps }) {
+	return (
+		<div className="scanner">
+			<img className="scanner__logo" src="/static/media/logo-app.8f17c777.svg" alt="" />
+			<div className="scanner__stepper">
+				{_.map(steps, step => {
+					return <Step key={step} step={step} selected={step === currentStep} />;
+				})}
+			</div>
+		</div>
+	);
 };
 
-const Step = ({ step, selected }) => <span className="type--wgt--regular u-mar--left--med">{selected ? <span className="type--wgt--bold type--color--note u-separator--primary">{step}</span> : step}</span>
+StepCounter.propTypes = {
+	currentStep: PropTypes.number.isRequired,
+	steps: PropTypes.array.isRequired,
+};
+
+const Step = ({ step, selected }) => (
+	<React.Fragment>
+		{selected ? (
+			<React.Fragment>
+				<div className="scanner__stepper__step scanner__stepper__step--active">{step}</div>
+				<div className="scanner__stepper__step--line"></div>
+			</React.Fragment>
+		) : (
+			<React.Fragment>
+				<div className="scanner__stepper__step">{step}</div>
+				<div className="scanner__stepper__step--line"></div>
+			</React.Fragment>
+		)}
+	</React.Fragment>
+);
 
 Step.propTypes = {
-    selected: PropTypes.bool.isRequired,
-    step: PropTypes.number.isRequired
+	selected: PropTypes.bool.isRequired,
+	step: PropTypes.number.isRequired,
 };
 
 export default defaultTemplate(StepCounter);
