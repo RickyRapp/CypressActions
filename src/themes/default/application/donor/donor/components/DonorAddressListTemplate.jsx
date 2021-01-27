2137cb3a75fee6cbd60312e19e403df163ebff1f
arajher <arajher@mono-software.com>
2021-01-27 15:19:41 +0100

    Style profile information with new data

--- src/themes/default/application/donor/donor/components/DonorAddressListTemplate.jsx
+++ src/themes/default/application/donor/donor/components/DonorAddressListTemplate.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
-import {
-    Address
-} from 'core/components';
+import { Address } from 'core/components';
 import { DonorAddressEditTemplate } from 'themes/application/donor/donor/components';
 
-const DonorAddressListTemplate = function ({ donorAddressViewStore, t }) {
-    const {
-        addresses,
-        onEnableEditClick,
-        onCancelEditClick,
-        isEditEnabled,
-        form,
-        editId
-    } = donorAddressViewStore;
+const DonorAddressListTemplate = function({ donorAddressViewStore, t }) {
+	const { addresses, onEnableEditClick, onCancelEditClick, isEditEnabled, form, editId } = donorAddressViewStore;
 
 	let primaryAddress = null;
 	let secondaryAddress = null;
@@ -25,56 +16,98 @@ const DonorAddressListTemplate = function ({ donorAddressViewStore, t }) {
 		}
 	}
 
-    return (
-        <div>
-            <div className="row u-mar--bottom--sml">
-                <div className="col col-sml-12 col-lrg-3">
-                    <h3 className="type--lrg type--wgt--medium u-mar--bottom--med">{t('DONOR.ACCOUNT_INFORMATION_FIELDS.TITLE_ADDRESS')}</h3>
-                </div>
-                <div
+	return (
+		<div>
+			<div className="row u-mar--bottom--sml">
+				<div className="col col-sml-12 col-lrg-3">
+					<h3 className="type--lrg type--wgt--medium u-mar--bottom--med">
+						{t('DONOR.ACCOUNT_INFORMATION_FIELDS.TITLE_ADDRESS')}
+					</h3>
+				</div>
+				<div
 					className={`col col-sml-12 col-lrg-${
 						(isEditEnabled && primaryAddress && primaryAddress.id === editId) || undefined === editId ? '12' : '9'
 					}`}
 				>
-                    <div className="row">
-                        <div className="col col-sml-12 col-lrg-12">
-                            {isEditEnabled && primaryAddress && primaryAddress.id === editId ?
-                                <DonorAddressEditTemplate
-                                    form={form}
-                                    title="Primary"
-                                    onCancelEditClick={onCancelEditClick}
-                                    isAssignableAsPrimary={false} />
-                                :
-                                <p
+					<div className="row">
+						<div className="col col-sml-12 col-lrg-12">
+							{isEditEnabled && primaryAddress && primaryAddress.id === editId ? (
+								<DonorAddressEditTemplate
+									form={form}
+									title="Primary"
+									onCancelEditClick={onCancelEditClick}
+									isAssignableAsPrimary={false}
+								/>
+							) : (
+								<div
 									className="type--base type--wgt--bold scale"
-                                    title='Click to edit'
-                                    onClick={() => onEnableEditClick(primaryAddress)}>
-                                    {primaryAddress ?
-                                        <Address value={primaryAddress} format='full' /> : ''}
-                                </p>}
-                        </div>
-                        <div className="col col-sml-12 col-lrg-12 u-mar--top--sml">
-                            {isEditEnabled && (secondaryAddress && secondaryAddress.id === editId || undefined === editId) ?
-                                <DonorAddressEditTemplate
-                                    form={form}
-                                    title="Secondary"
-                                    onCancelEditClick={onCancelEditClick}
-                                    isAssignableAsPrimary={true} />
-                                :
-                                <span
-                                    title={`Click to ${secondaryAddress ? 'edit' : 'insert'}`}
-                                    onClick={() => onEnableEditClick(secondaryAddress)}>
-                                    {secondaryAddress ?
-                                        <Address value={secondaryAddress} format='full' />
-                                        :
-                                        <button className="btn btn--link btn--sml">Add new address</button>}
-                                </span>}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+									title="Click to edit"
+									onClick={() => onEnableEditClick(primaryAddress)}
+								>
+									{primaryAddress ? (
+										// <Address value={primaryAddress} format='full' />
+										<div className="row">
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">
+													Address Line 1:
+												</p>
+												<p className="type--base type--wgt--bold"> {primaryAddress.addressLine1} </p>
+											</div>
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">
+													Address Line 2:
+												</p>
+												<p className="type--base type--wgt--bold"> {primaryAddress.addressLine2} </p>
+											</div>
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">City:</p>
+												<p className="type--base type--wgt--bold">{primaryAddress.city}</p>
+											</div>
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">State:</p>
+												<p className="type--base type--wgt--bold">{primaryAddress.state}</p>
+											</div>
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Zip Code:</p>
+												<p className="type--base type--wgt--bold">{primaryAddress.zipCode}</p>
+											</div>
+											<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+												<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Is primary?</p>
+												<p className="type--base type--wgt--bold">{primaryAddress.isPrimary ? "Yes" : "No"}</p>
+											</div>
+										</div>
+									) : (
+										''
+									)}
+								</div>
+							)}
+						</div>
+						<div className="col col-sml-12 col-lrg-12 u-mar--top--sml">
+							{isEditEnabled && ((secondaryAddress && secondaryAddress.id === editId) || undefined === editId) ? (
+								<DonorAddressEditTemplate
+									form={form}
+									title="Secondary"
+									onCancelEditClick={onCancelEditClick}
+									isAssignableAsPrimary={true}
+								/>
+							) : (
+								<span
+									title={`Click to ${secondaryAddress ? 'edit' : 'insert'}`}
+									onClick={() => onEnableEditClick(secondaryAddress)}
+								>
+									{secondaryAddress ? (
+										<Address value={secondaryAddress} format="full" />
+									) : (
+										<button className="btn btn--link btn--sml">Add new address</button>
+									)}
+								</span>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 DonorAddressListTemplate.propTypes = {
--- src/themes/default/application/donor/donor/components/DonorEmailAddressListTemplate.jsx
+++ src/themes/default/application/donor/donor/components/DonorEmailAddressListTemplate.jsx
@@ -4,77 +4,110 @@ import { defaultTemplate } from 'core/hoc';
 import { DonorEmailAddressEditTemplate } from 'themes/application/donor/donor/components';
 import { EmailAddress } from 'core/components';
 
-const DonorEmailAddressListTableTemplate = function ({ donorEmailAddressViewStore, t }) {
-    const {
-        emailAddresses,
-        onEnableEditClick,
-        onCancelEditClick,
-        isEditEnabled,
-        form,
-        editId
-    } = donorEmailAddressViewStore;
+const DonorEmailAddressListTableTemplate = function({ donorEmailAddressViewStore, t }) {
+	const {
+		emailAddresses,
+		onEnableEditClick,
+		onCancelEditClick,
+		isEditEnabled,
+		form,
+		editId,
+	} = donorEmailAddressViewStore;
 
-    let primaryEmailAddress = null;
-    let secondaryEmailAddress = null;
-    if (emailAddresses.length > 0) {
-        primaryEmailAddress = emailAddresses.find(c => c.isPrimary);
-        if (emailAddresses.some(c => !c.isPrimary)) {
-            secondaryEmailAddress = emailAddresses.find(c => !c.isPrimary);
-        }
+	let primaryEmailAddress = null;
+	let secondaryEmailAddress = null;
+	if (emailAddresses.length > 0) {
+		primaryEmailAddress = emailAddresses.find(c => c.isPrimary);
+		if (emailAddresses.some(c => !c.isPrimary)) {
+			secondaryEmailAddress = emailAddresses.find(c => !c.isPrimary);
+		}
     }
-
-    return (
-        <div>
-            <div className="row u-mar--bottom--sml">
-                <div className="col col-sml-12 col-lrg-3">
-                    <h3 className="type--lrg type--wgt--medium u-mar--bottom--med">{t('DONOR.ACCOUNT_INFORMATION_FIELDS.TITLE_EMAIL_ADDRESS')}</h3>
-                </div>
-                <div className={`col col-sml-12 col-lrg-${isEditEnabled && primaryEmailAddress && primaryEmailAddress.id === editId || (secondaryEmailAddress && secondaryEmailAddress.id === editId || undefined === editId) ? "12" : "9"}`}>
-                    <div className="row u-mar--bottom--sml">
-                        <div className="col col-sml-12 col-lrg-12">
-                            {isEditEnabled && primaryEmailAddress && primaryEmailAddress.id === editId ?
-                                <DonorEmailAddressEditTemplate
-                                    form={form}
-                                    title="Primary"
-                                    onCancelEditClick={onCancelEditClick}
-                                    isAssignableAsPrimary={false} />
-                                :
-                                <p
-                                className="type--base type--wgt--bold scale"
-                                    title='Click to edit'
-                                    onClick={() => onEnableEditClick(primaryEmailAddress)}>
-                                    {primaryEmailAddress ?
-                                        <EmailAddress value={primaryEmailAddress} format='full' /> : ''}
-                                </p>}
-                        </div>
-                        <div className="col col-sml-12 col-lrg-12 u-mar--top--sml">
-                            {isEditEnabled && (secondaryEmailAddress && secondaryEmailAddress.id === editId || undefined === editId) ?
-                                <DonorEmailAddressEditTemplate
-                                    form={form}
-                                    title="Secondary"
-                                    onCancelEditClick={onCancelEditClick}
-                                    isAssignableAsPrimary={true} />
-                                :
-                                <span
-                                    title={`Click to ${secondaryEmailAddress ? 'edit' : 'insert'}`}
-                                    onClick={() => onEnableEditClick(secondaryEmailAddress)}>
-                                    {secondaryEmailAddress ?
-                                        <EmailAddress value={secondaryEmailAddress} format='full' />
-                                        :
-                                        <button className="btn btn--link btn--sml">Add new email address</button>}
-                                </span>}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div >
-    )
+    
+	return (
+		<div>
+			<div className="row u-mar--bottom--sml">
+				<div className="col col-sml-12 col-lrg-3">
+					<h3 className="type--lrg type--wgt--medium u-mar--bottom--med">
+						{t('DONOR.ACCOUNT_INFORMATION_FIELDS.TITLE_EMAIL_ADDRESS')}
+					</h3>
+				</div>
+				<div
+					className={`col col-sml-12 col-lrg-${
+						(isEditEnabled && primaryEmailAddress && primaryEmailAddress.id === editId) ||
+						((secondaryEmailAddress && secondaryEmailAddress.id === editId) || undefined === editId)
+							? '12'
+							: '9'
+					}`}
+				>
+					<div className="row u-mar--bottom--sml">
+						<div className="col col-sml-12 col-lrg-12">
+							{isEditEnabled && primaryEmailAddress && primaryEmailAddress.id === editId ? (
+								<DonorEmailAddressEditTemplate
+									form={form}
+									title="Primary"
+									onCancelEditClick={onCancelEditClick}
+									isAssignableAsPrimary={false}
+								/>
+							) : (
+								<div
+									className="type--base type--wgt--bold scale"
+									title="Click to edit"
+									onClick={() => onEnableEditClick(primaryEmailAddress)}
+								>
+									{primaryEmailAddress ? (
+										// <EmailAddress value={primaryEmailAddress} format='full' />
+										<div className="row">
+                                            <div className="col col-sml-12 col-lrg-4">
+											    <p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Email:</p>
+                                                <p className="type--base type--wgt--bold"> {primaryEmailAddress.email} </p>
+                                            </div>
+                                            <div className="col col-sml-12 col-lrg-4">
+											    <p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Is notify enable?</p>
+                                                <p className="type--base type--wgt--bold">{primaryEmailAddress.isNotifyEnabled ? "Yes" : "No"}</p>
+                                            </div>
+                                            <div className="col col-sml-12 col-lrg-4">
+                                                <p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Is primary?</p>
+                                                <p className="type--base type--wgt--bold">{primaryEmailAddress.isPrimary ? "Yes" : "No"}</p>
+                                            </div>
+										</div>
+									) : (
+										''
+									)}
+								</div>
+							)}
+						</div>
+						<div className="col col-sml-12 col-lrg-12 u-mar--top--sml">
+							{isEditEnabled &&
+							((secondaryEmailAddress && secondaryEmailAddress.id === editId) || undefined === editId) ? (
+								<DonorEmailAddressEditTemplate
+									form={form}
+									title="Secondary"
+									onCancelEditClick={onCancelEditClick}
+									isAssignableAsPrimary={true}
+								/>
+							) : (
+								<span
+									title={`Click to ${secondaryEmailAddress ? 'edit' : 'insert'}`}
+									onClick={() => onEnableEditClick(secondaryEmailAddress)}
+								>
+									{secondaryEmailAddress ? (
+										<EmailAddress value={secondaryEmailAddress} format="full" />
+									) : (
+										<button className="btn btn--link btn--sml">Add new email address</button>
+									)}
+								</span>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 DonorEmailAddressListTableTemplate.propTypes = {
-    donorEmailAddressViewStore: PropTypes.object.isRequired,
-    t: PropTypes.func.isRequired
+	donorEmailAddressViewStore: PropTypes.object.isRequired,
+	t: PropTypes.func.isRequired,
 };
 
 export default defaultTemplate(DonorEmailAddressListTableTemplate);
-
