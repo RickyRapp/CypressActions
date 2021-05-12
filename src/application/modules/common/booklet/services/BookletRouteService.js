8caff405ac43d6021f21b625a4d65ed37857fd4d
Miroslav Stojanovic <mstojanovic@mono-software.com>
2021-05-12 13:34:03 +0200

    Ability to search by donors name

--- src/application/modules/common/booklet/services/BookletRouteService.js
+++ src/application/modules/common/booklet/services/BookletRouteService.js
@@ -6,7 +6,7 @@ class BookletRouteService extends BaseRouteService {
     }
 
     find(filter) {
-        return super.find(this.base + '/{?donorId,exceptIds,trackingNumber,deliveryMethodTypeIds,bookletStatusIds,bookletTypeIds,denominationTypeIds,codes,certificateBarcodes,dateCreatedStartDate,dateCreatedEndDate,page,rpp,sort,embed,fields}', filter);
+        return super.find(this.base + '/{?donorId,exceptIds,trackingNumber,deliveryMethodTypeIds,bookletStatusIds,bookletTypeIds,denominationTypeIds,donorsName,codes,certificateBarcodes,dateCreatedStartDate,dateCreatedEndDate,page,rpp,sort,embed,fields}', filter);
     }
 
     get(id, options) {
