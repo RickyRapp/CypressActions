4ac8eee6eea2648cb570fd3883062c2fe12d5c9a
Bojan Ilic <bilic@mono-software.com>
2021-08-12 07:52:59 +0200

    Plaid remove from develop branch

--- src/themes/default/application/administration/dashboard/pages/DashboardTemplate.jsx
+++ src/themes/default/application/administration/dashboard/pages/DashboardTemplate.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Page } from 'core/layouts';
 import { defaultTemplate } from 'core/hoc';
 import PropTypes from 'prop-types';
-import CharityPlaid from 'application/charity/charity/components/CharityPlaid';
 
 function DashboardTemplate() {
 	return (
@@ -11,7 +10,6 @@ function DashboardTemplate() {
 				<div className="col col-sml-12 col-xxlrg-6 u-mar--bottom--med">
 					admin
 				</div>
-				<CharityPlaid/>
 			</div>
 		</Page>
 	);
