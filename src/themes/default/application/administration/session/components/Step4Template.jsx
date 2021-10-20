f74a3a5b95efe7141baf73208d2fabe24bdbd32f
MONO\lmihic <lmihic@mono-software.com>
2021-10-20 16:23:01 +0200

    session step 4 summary

--- src/themes/default/application/administration/session/components/Step4Template.jsx
+++ src/themes/default/application/administration/session/components/Step4Template.jsx
@@ -4,7 +4,6 @@ import { defaultTemplate } from 'core/hoc';
 import { BaasicButton, FormatterResolver } from 'core/components';
 
 function Step4Template({ t, onNextStepClick, currentCount, session, sessionCertificates }) {
-
 	return (
 		<React.Fragment>
 			<div className="scanner__finish--card">
@@ -16,7 +15,11 @@ function Step4Template({ t, onNextStepClick, currentCount, session, sessionCerti
 						</span>
 						<span className="type--med type--wgt--bold u-push">
 							{t('SESSION.CREATE.STEP4.TOTAL')}:{' '}
-							<span className="type--wgt--bold u-mar--left--sml">${session && session.amount}</span>
+							<span className="type--wgt--bold u-mar--left--sml">{sessionCertificates.length > 0 && <FormatterResolver
+										item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
+										field='amount'
+										format={{ type: 'currency' }}
+									/>}</span>
 						</span>
 					</div>
 					<div className="row col col-lrg-12 card--primary card--med type--base type--wgt--regular u-mar--bottom--sml">
@@ -24,24 +27,53 @@ function Step4Template({ t, onNextStepClick, currentCount, session, sessionCerti
 									Checks scanned
 								</div>
 								<div className="col col-sml-6 type--sml type--wgt--medium">{`${sessionCertificates.length}`}</div>
-								<div className="col col-sml-6 type--base type--wgt--medium">{t('GRANT.PREVIEW.FIELDS.AMOUNT_LABEL')}</div>
+								
+								<div className="col col-sml-6 type--base type--wgt--medium">
+									Fees
+								</div>
 								<span className="col col-sml-6 input--preview">
 									{sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
-										item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
+										item={{ amount: sessionCertificates.map(c => (c.isBlank ? c.certificateValue : (c.denominationTypeValue - c.certificateValue))).reduce((a, b) => a + b) }}
 										field='amount'
 										format={{ type: 'currency' }}
 									/>}
 								</span>
+
+								{
+									sessionCertificates.filter(x => x.insufficientFunds).length > 0 ?
+									<div>
+										<div className="col col-sml-6">
+											Insufficient checks
+										</div>
+										<div className="col col-sml-6">
+											{sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
+												item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
+												field='amount'
+												format={{ type: 'currency' }}
+											/>}
+										</div>
+									</div> : 
+									<div>
+										<div className="col col-sml-6">
+											Insufficient checks
+										</div>
+										<div className="col col-sml-6">
+											No insufficient checks
+										</div>
+									</div>
+								}
+								
 								<div className="col col-sml-6">
-									Insufficient checks
+									<span className=" type--color--note">Grand total</span> (before insufficient funds)
 								</div>
 								<div className="col col-sml-6">
-									{sessionCertificates.length > 0 && !sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
-										item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
+									{sessionCertificates.length > 0 && <FormatterResolver
+										item={{ amount: sessionCertificates.map(c => c.denominationTypeValue).reduce((a, b) => a + b) }}
 										field='amount'
 										format={{ type: 'currency' }}
 									/>}
 								</div>
+
 								<div className="col col-sml-6">
 									<span className=" type--color--note">Grand total</span> (including insufficient checks)
 								</div>
@@ -78,7 +110,6 @@ function Step4Template({ t, onNextStepClick, currentCount, session, sessionCerti
 }
 
 Step4Template.propTypes = {
-	step4ViewStore: PropTypes.object.isRequired,
 	t: PropTypes.func.isRequired,
 	onNextStepClick: PropTypes.func.isRequired,
 	session: PropTypes.object.isRequired,
