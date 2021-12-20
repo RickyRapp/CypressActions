10151f471b0e4a031c851284b88a5fce98753e61
MONO\arajher <arajher@mono-software.com>
2021-12-20 11:29:09 +0100

    change toggle component and fix styling

--- src/themes/default/components/checkbox/BaasicToggleTemplate.jsx
+++ src/themes/default/components/checkbox/BaasicToggleTemplate.jsx
@@ -5,24 +5,8 @@ import { BaasicButton } from 'core/components';
 
 function BaasicToggleTemplate({ label, value, onChange, disabled, t, showLabel, wrapperClassName }) {
 	return (
-		<div className={`${wrapperClassName ? wrapperClassName : "u-display--flex"}`}>
-			<div className="">
-				<div className={`toggle ${value ? 'toggle--active' : 'toggle--deactive'}`}>
-					<BaasicButton
-						className={`toggle__btn ${value ? '' : 'toggle__btn--active'}`}
-						onClick={onChange}
-						disabled={disabled}
-						label={null}
-					/>
-
-					<BaasicButton
-						className={`toggle__btn ${value ? 'toggle__btn--active' : ''}`}
-						onClick={onChange}
-						disabled={disabled}
-						label={null}
-					/>
-				</div>
-			</div>
+		<div className={`${wrapperClassName ? wrapperClassName : "toggle__wrapper"}`}>
+			<input className={`input toggle ${value ? 'toggle--active' : ''}`} type="checkbox" onClick={onChange} />
 			<div className="u-mar--left--sml">
 				<span>{showLabel ? t(label) : null}</span>
 			</div>
@@ -36,8 +20,21 @@ BaasicToggleTemplate.propTypes = {
 	disabled: PropTypes.bool,
 	showLabel: PropTypes.bool,
 	label: PropTypes.string,
-    wrapperClassName: PropTypes.string,
+	wrapperClassName: PropTypes.string,
 	t: PropTypes.func,
 };
 
 export default defaultTemplate(BaasicToggleTemplate);
+
+{/* <BaasicButton
+	className={`toggle__btn ${value ? '' : 'toggle__btn--active'}`}
+	onClick={onChange}
+	disabled={disabled}
+	label={null}
+/>
+
+<BaasicButton
+	className={`toggle__btn ${value ? 'toggle__btn--active' : ''}`}
+	disabled={disabled}
+	label={null}
+/> */}
