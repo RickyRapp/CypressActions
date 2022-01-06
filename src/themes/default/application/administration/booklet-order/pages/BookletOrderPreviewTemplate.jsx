4966fa7009eb579ad7738843e65fb55947829254
MONO\arajher <arajher@mono-software.com>
2022-01-06 18:37:24 +0100

    fix responisve in grant details

--- src/themes/default/application/administration/booklet-order/pages/BookletOrderPreviewTemplate.jsx
+++ src/themes/default/application/administration/booklet-order/pages/BookletOrderPreviewTemplate.jsx
@@ -18,53 +18,55 @@ const BookletOrderPreviewTemplate = function ({ bookletOrderPreviewViewStore, t 
 			loading={loaderStore.loading}
 			layoutFooterVisible={false}
 		>
-			<div className="row">
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DONOR_LABEL')}</div>
-					<span className="input--preview">
-						{item && item.donor.donorName}
-					</span>
+			<div className="card--primary card--med u-mar--bottom--base container--sml">
+				<div className="row">
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DONOR_LABEL')}</div>
+						<span className="input--preview">
+							{item && item.donor.donorName}
+						</span>
+					</div>
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.STATUS_LABEL')}</div>
+						<span className="input--preview">
+							{item && item.bookletOrderStatus.name}
+						</span>
+					</div>
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DATE_CREATED_LABEL')}</div>
+						<span className="input--preview">
+							{item && <Date format="full" value={item.dateCreated} />}
+						</span>
+					</div>
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DATE_UPDATED_LABEL')}</div>
+						<span className="input--preview">
+							{item && <Date format="full" value={item.dateUpdated} />}
+						</span>
+					</div>
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.CONFRIMATION_NUMBER_LABEL')}</div>
+						<span className="input--preview">
+							{item && item.confirmationNumber}
+						</span>
+					</div>
+					<div className="col col-sml-12 col-med-6 u-mar--bottom--sml">
+						<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DELIVERY_METHOD_LABEL')}</div>
+						<span className="input--preview">
+							{item && item.deliveryMethodType.name}
+							{item && (item.deliveryMethodType.abrv === 'mail-usps' || item.deliveryMethodType.abrv === 'express-mail') &&
+								<span>- {item.trackingNumber}</span>}
+						</span>
+					</div>
 				</div>
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.STATUS_LABEL')}</div>
-					<span className="input--preview">
-						{item && item.bookletOrderStatus.name}
-					</span>
-				</div>
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DATE_CREATED_LABEL')}</div>
-					<span className="input--preview">
-						{item && <Date format="full" value={item.dateCreated} />}
-					</span>
-				</div>
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DATE_UPDATED_LABEL')}</div>
-					<span className="input--preview">
-						{item && <Date format="full" value={item.dateUpdated} />}
-					</span>
-				</div>
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.CONFRIMATION_NUMBER_LABEL')}</div>
-					<span className="input--preview">
-						{item && item.confirmationNumber}
-					</span>
-				</div>
-				<div className="col col-sml-12 col-lrg-3 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.DELIVERY_METHOD_LABEL')}</div>
-					<span className="input--preview">
-						{item && item.deliveryMethodType.name}
-						{item && (item.deliveryMethodType.abrv === 'mail-usps' || item.deliveryMethodType.abrv === 'express-mail') &&
-							<span>- {item.trackingNumber}</span>}
-					</span>
-				</div>
-				<div className="col col-sml-12 col-lrg-12 u-mar--bottom--sml">
-					<div className="type--base type--wgt--medium type--color--note">{t('BOOKLET_ORDER.PREVIEW.ORDER_LABEL')}</div>
-					<span className="input--preview">
-						<SimpleBaasicTable
-							tableStore={tableStore}
-						/>
-					</span>
-				</div>
+			</div>
+			<div>
+				<div className="type--med type--wgt--medium">{t('BOOKLET_ORDER.PREVIEW.ORDER_LABEL')}</div>
+				<span className="input--preview">
+					<SimpleBaasicTable
+						tableStore={tableStore}
+					/>
+				</span>
 			</div>
 		</PreviewLayout>
 	);
