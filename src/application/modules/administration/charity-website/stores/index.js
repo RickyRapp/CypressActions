d68bbf495df58deff676d29fb90476e8fe5e20e8
MONO\isusac <isusac@mono.software>
2022-01-26 10:07:43 +0100

    Created tab view for Charity website

--- src/application/modules/administration/charity-website/stores/index.js
+++ src/application/modules/administration/charity-website/stores/index.js
@@ -1,3 +1,4 @@
 export { default as CharityWebsiteViewStore } from './CharityWebsiteViewStore'
 export { default as CharityWebsiteCreateViewStore } from './CharityWebsiteCreateViewStore'
 export { default as CharityWebsiteStore } from './CharityWebsiteStore'
+export { default as CharitiyWebsiteTabViewStore } from './CharitiyWebsiteTabViewStore'
--- src/themes/default/application/administration/charity-website/pages/index.js
+++ src/themes/default/application/administration/charity-website/pages/index.js
@@ -1,1 +1,2 @@
 export { default as CharityWebsiteListTemplate } from './CharityWebsiteListTemplate'
+export { default as CharityWebsiteTabTemplate } from './CharityWebsiteTabTemplate'
