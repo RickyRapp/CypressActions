97c7b19ee406668c461f69dd53d8c331d0b1aad7
MONO\arajher <arajher@mono-software.com>
2022-02-03 11:24:54 +0100

    style order checks

--- src/themes/default/application/common/booklet-order/pages/BookletOrderCreateTemplate.jsx
+++ src/themes/default/application/common/booklet-order/pages/BookletOrderCreateTemplate.jsx
@@ -8,7 +8,7 @@ import {
     BaasicFormControls,
     Address,
     BaasicModal,
-   //BaasicFieldDropdown
+    //BaasicFieldDropdown
 } from 'core/components';
 
 import { defaultTemplate } from 'core/hoc';
@@ -195,10 +195,20 @@ const BookletOrderCreateTemplate = function ({ store, t }) {
                                         </div>
                                     )
                                 })}
+                                <div className="u-mar--top--sml u-mar--bottom--sml">
+                                    <div className="type--center">
+                                        <button type="button" className="btn btn--show type--wgt--medium" onClick={onShowMoreOptionsClick}>
+                                            <i className={!showMoreOptions ? "u-icon u-icon--base u-icon--arrow-down--primary" : "u-icon u-icon--base u-icon--arrow-down--primary u-rotate--180"}></i>
+                                            {showMoreOptions ? t('BOOKLET_ORDER.CREATE.HIDE_MORE_OPTIONS') : t('BOOKLET_ORDER.CREATE.SHOW_MORE_OPTIONS')}
+                                            <i className={!showMoreOptions ? "u-icon u-icon--base u-icon--arrow-down--primary" : "u-icon u-icon--base u-icon--arrow-down--primary u-rotate--180"}></i>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
+
                             <div className="col col-sml-12 col-xxxlrg-4 u-mar--bottom--med u-order--3--xxxlrg">
                                 <div className="card--sml card--secondary type--center">
-                                    <h2 className="type--xlrg type--wgt--medium type--color--note"> <span className="type--med type--color--text">Balance: </span>
+                                    <h2 className="type--xlrg type--wgt--medium type--color--note"> <span className="type--med type--color--text">Available balance: </span>
                                         {donor && <FormatterResolver
                                             item={{ availableBalance: donor.availableBalance }}
                                             field='availableBalance'
@@ -264,7 +274,7 @@ const BookletOrderCreateTemplate = function ({ store, t }) {
                                                 </tr>
                                             </tfoot>
                                         }
-                                        
+
                                         <tfoot>
                                             <tr>
                                                 <th colSpan="2">Shipping method</th>
@@ -278,25 +288,51 @@ const BookletOrderCreateTemplate = function ({ store, t }) {
                                             </tr>
                                         </tfoot>
                                         {donor && donor.accountType && donor.accountType.abrv != 'private' && ((form.$('customizedName').value && form.$('customizedName').value.length > 0) || (form.$('customizedAddressLine1').value && form.$('customizedAddressLine1').value.length > 0))
-                                        ? <tfoot>
-                                            <tr>
-                                                <th colSpan="2">Custom booklet fee</th>
-                                                <th>${(orderContents.reduce((a,b) => a+b.bookletCount, 0) * 5).toFixed(2)}</th>
-                                            </tr>
-                                        </tfoot> : null}
-                                        
+                                            ? <tfoot>
+                                                <tr>
+                                                    <th colSpan="2">Custom booklet fee</th>
+                                                    <th>${(orderContents.reduce((a, b) => a + b.bookletCount, 0) * 5).toFixed(2)}</th>
+                                                </tr>
+                                            </tfoot> : null}
+
                                     </table>
 
                                 </div>
-                            </div>
 
-                            <div className="col col-sml-12 u-mar--top--sml u-mar--bottom--sml u-order--2--xxxlrg">
-                                <div className="type--center">
-                                    <button type="button" className="btn btn--show type--wgt--medium" onClick={onShowMoreOptionsClick}>
-                                        <i className={!showMoreOptions ? "u-icon u-icon--base u-icon--arrow-down--primary" : "u-icon u-icon--base u-icon--arrow-down--primary u-rotate--180"}></i>
-                                        {showMoreOptions ? t('BOOKLET_ORDER.CREATE.HIDE_MORE_OPTIONS') : t('BOOKLET_ORDER.CREATE.SHOW_MORE_OPTIONS')}
-                                        <i className={!showMoreOptions ? "u-icon u-icon--base u-icon--arrow-down--primary" : "u-icon u-icon--base u-icon--arrow-down--primary u-rotate--180"}></i>
-                                    </button>
+                                {donor && !donor.hasProtectionPlan &&
+                                    <div className="u-mar--top--med">
+                                        <div className="message--enh">
+                                            <span className="u-mar--right--tny">
+                                                {t('BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN_MESSAGE')}
+                                            </span>
+                                            <a href="#" className="u-anchor--underline" onClick={onAddProtectionPlanClick}>
+                                                {t('BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN')}
+                                            </a>
+                                            {/* <BaasicButton
+                                            className="btn btn--link"
+                                            label={'BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN'}
+                                            >
+                                        </BaasicButton> */}
+                                        </div>
+                                    </div>
+                                }
+
+                                <div className="type--med type--wgt--medium type--right u-push--from--lrg u-mar--top--med ">
+                                    <div className="u-display--flex u-display--flex--justify--flex-end u-display--flex--align--baseline">
+                                        <span>
+                                            Total:
+                                        </span>
+                                        <span className="type--xlrg type--wgt--medium type--color--note u-mar--left--sml u-push--to--lrg">
+                                            <FormatterResolver
+                                                item={{ total: totalAmount }}
+                                                field='total'
+                                                format={{ type: 'currency' }}
+                                            />
+                                        </span>
+                                    </div>
+
+                                    {donor && donor.hasProtectionPlan &&
+                                        <div><small>{t('BOOKLET_ORDER.CREATE.ENROLLED_IN_PROTECTION_PLAN')}<i className="u-icon u-icon--approve u-icon--base u-mar--left--sml"></i></small></div>}
                                 </div>
                             </div>
 
@@ -356,67 +392,26 @@ const BookletOrderCreateTemplate = function ({ store, t }) {
                             </div>
                         */}
                         </div>
-
-                        <div className="card--sml">
-                            {donor && !donor.hasProtectionPlan &&
-                                <div className="u-mar--bottom--med">
-                                    <div className="message--enh">
-                                        <span className="u-mar--right--tny">
-                                            {t('BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN_MESSAGE')}
-                                        </span>
-                                        <a href="#" className="u-anchor--underline" onClick={onAddProtectionPlanClick}>
-                                            {t('BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN')}
-                                        </a>
-                                        {/* <BaasicButton
-                                            className="btn btn--link"
-                                            label={'BOOKLET_ORDER.CREATE.BUTTON.ADD_PROTECTION_PLAN'}
-                                            >
-                                        </BaasicButton> */}
-                                    </div>
-                                </div>
-                            }
-                            <div className="row row--form u-display--flex u-display--flex--align--end">
-                                <div className="col col-sml-12 col-xlrg-8">
-                                    <div className="type--sml type--wgt--medium">
-                                        Current Balance:
-                                        <span className="type--med type--color--note u-mar--left--sml u-push--to--lrg">
-                                            {donor && <FormatterResolver
-                                                item={{ availableBalance: donor.availableBalance }}
-                                                field='availableBalance'
-                                                format={{ type: 'currency' }}
-                                            />}
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className="col col-sml-12 col-xlrg-4">
-                                    <div className="type--med type--wgt--medium u-push--from--lrg">Total:
-                                        <span className="type--xlrg type--wgt--medium type--color--note u-mar--left--sml u-push--to--lrg">
-                                            <FormatterResolver
-                                                item={{ total: totalAmount }}
-                                                field='total'
-                                                format={{ type: 'currency' }}
-                                            />
-                                        </span>
-
-                                        {donor && donor.hasProtectionPlan &&
-                                            <div><small>{t('BOOKLET_ORDER.CREATE.ENROLLED_IN_PROTECTION_PLAN')}<i className="u-icon u-icon--approve u-icon--base u-mar--left--sml"></i></small></div>}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
 
                     <div className="card--primary card--med u-mar--bottom--med">
                         <div className="row row--form u-display--flex u-display--flex--align--center">
-                            <div className="col col-sml-12 col-med-12 col-lrg-2 u-mar--bottom--sml">
+                            <div className="col col-sml-12 col-med-12 col-xxlrg-2 u-mar--bottom--sml">
                                 <span className="type--med type--wgt--medium type--color--note">
                                     {t('BOOKLET_ORDER.CREATE.DELIVERY_OPTIONS')}
                                 </span>
                             </div>
                             {deliveryMethodTypes.map(c => {
+                                let className = "";
+                                if (c.sortOrder === 0) {
+                                    className = "col-sml-12 col-med-3 col-xxlrg-2"
+                                } else if (c.sortOrder === 1) {
+                                    className = "col-sml-12 col-med-5 col-xxlrg-3"
+                                } else {
+                                    className = "col-sml-12 col-med-3 col-xxlrg-2"
+                                }
                                 return (
-                                    <div key={c.id} className="col col-sml-12 col-med-12 col-lrg-3 u-mar--bottom--sml">
+                                    <div key={c.id} className={`col ${className} u-mar--bottom--sml`}>
                                         <BasicRadio
                                             label={c.abrv === 'express-mail' ? `${c.name} (Additional $25)` : c.name}
                                             value={c.id}
