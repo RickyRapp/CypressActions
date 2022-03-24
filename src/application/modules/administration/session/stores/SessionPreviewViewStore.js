65ce8c3011fd311f71b26c9cc05e850f9f80fee7
Luka Mihic <lmihic@mono-software.com>
2022-03-24 14:03:48 +0100

    #87183 - session amount total

--- src/application/modules/administration/session/stores/SessionPreviewViewStore.js
+++ src/application/modules/administration/session/stores/SessionPreviewViewStore.js
@@ -33,7 +33,7 @@ class SessionPreviewViewStore extends BasePreviewViewStore {
                             ]
                         }
                         const data = await rootStore.application.administration.sessionStore.getSession(id, params);
-                        data.grants = data.grants.filter(c => (c.certificate.openCertificateAmount && c.certificate.isBlankApprovedByAdmin) || !c.certificate.openCertificateAmount || (c.certificate.openCertificateAmount && (c.certificate.needsAdminReview == null || c.certificate.needsAdminReview == false)))
+                        data.grants = data.grants.filter(c => c.certificate.isBlankApprovedByAdmin != false && (c.certificate.openCertificateAmount && c.certificate.isBlankApprovedByAdmin) || !c.certificate.openCertificateAmount || (c.certificate.openCertificateAmount && (c.certificate.needsAdminReview == null || c.certificate.needsAdminReview == false)))
                         const discarded = await rootStore.application.administration.sessionStore.getSession(id, params);
                         discarded.grants = discarded.grants.filter(c => (c.certificate.openCertificateAmount && c.certificate.isBlankApprovedByAdmin == false))
                         const pending = await rootStore.application.administration.sessionStore.getSession(id, params);
