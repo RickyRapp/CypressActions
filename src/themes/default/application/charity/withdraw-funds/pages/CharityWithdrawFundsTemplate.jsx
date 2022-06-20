08519a87b7fa103bbb78b0e55f2a921508da4493
MONO\arajher <arajher@mono-software.com>
2022-06-20 14:50:18 +0200

    added styling for withdraw card

--- src/themes/default/application/charity/withdraw-funds/pages/CharityWithdrawFundsTemplate.jsx
+++ src/themes/default/application/charity/withdraw-funds/pages/CharityWithdrawFundsTemplate.jsx
@@ -4,14 +4,15 @@ import { defaultTemplate } from 'core/hoc';
 import { Content, Page } from 'core/layouts';
 
 function CharityWithdrawFundsTemplate({ charityWithdrawFundsViewStore, t }) {
-	const { 
-
-     } = charityWithdrawFundsViewStore;
+	// const {} = charityWithdrawFundsViewStore;
 
 	return (
 		<Page>
-            <div>
-                ok
+            <div className="card--primary card--med withdraw__container">
+                <p className="withdraw__title">Available Balance</p>
+                <p className="withdraw__price">$50,000</p>
+				<p className="type--lrg">How much would you like to withdraw?</p>
+				<p className="type--sml type--color--opaque u-mar--bottom--sml">Minimum $500 investment required.</p>
             </div>
         </Page>
 	);
