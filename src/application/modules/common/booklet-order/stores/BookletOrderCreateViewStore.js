5eb60741d1f9772a18fa6bba9f9af97668ae9629
Luka Mihic <lmihic@mono-software.com>
2022-06-20 15:38:08 +0200

    ﻿minor change in custom fee

--- src/application/modules/common/booklet-order/stores/BookletOrderCreateViewStore.js
+++ src/application/modules/common/booklet-order/stores/BookletOrderCreateViewStore.js
@@ -186,7 +186,13 @@ class BookletOrderCreateViewStore extends BaseEditViewStore {
     }
     
     @computed get customizedFee() {
-        return this.orderContents.reduce((a,b) => a+b.bookletCount, 0)*5;
+        var oneDollar = this.denominationTypes.find(x => x.abrv == 'one');
+        var twoDollar = this.denominationTypes.find(x => x.abrv == 'two');
+        var threeDollar = this.denominationTypes.find(x => x.abrv == 'three');
+        var fiveDollar = this.denominationTypes.find(x => x.abrv == 'five');
+        if(this.form.$('customizedName').length > 0 || this.form.$('customizedAddressLine1').length > 0 || this.form.$('customizedCity').length > 0 || this.form.$('customizedZipCode').length > 0)
+            return this.orderContents.reduce((a, b) => a + b.bookletCount, 0) * 5;
+        return this.orderContents.filter(x => x.denominationTypeId != oneDollar.id && x.denominationTypeId != twoDollar.id && x.denominationTypeId != threeDollar.id && x.denominationTypeId != fiveDollar.id).reduce((a, b) => a + b.bookletCount, 0) * 5;
     }
 
     @computed get totalPrepaidAmount() {
