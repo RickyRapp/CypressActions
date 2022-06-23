7df9d64b718626c7a3dbc529b7444bb2d1f3513a
Ricky <rickyrappaport10@gmail.com>
2022-06-23 10:40:33 -0500

    updated config again to include ignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+  e2e: { 
+    ignoreTestFiles: ['1-getting-started/', '2-advanced-examples/'], 
    // baseUrl:
     //  'http://localhost:3000/',
       // 'https://restaurants-clientversion.herokuapp.com/'),
@@ -13,8 +14,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-
+  }, 
   component: {
     devServer: {
       framework: "create-react-app",
