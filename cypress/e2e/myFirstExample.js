068f6f20991e4bcdfcf387b745dd112f2c4c23d6
Ricky <rickyrappaport10@gmail.com>
2022-06-23 11:41:26 -0500

    removed unneccessary files

--- cypress/e2e/myFirstExample.js
+++
@@ -1,9 +0,0 @@
-context('My First Example', () => {
-    beforeEach(() => {
-      cy.visit('https://example.cypress.io/commands/actions')
-    })
-
-    it('has an h1 on the page', () => {
-        cy.get('h1').should('exist');
-    })
-})
