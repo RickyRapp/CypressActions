6257e1bcf3f31ed56fe606b987217e537e40f38c
Ilija Sušac <isusac@mono.software>
2022-07-11 13:38:43 +0200

    Dispaly Withdraw flag on reconcile page and display detailed

--- src/application/modules/administration/reconcile/stores/ReconcileViewStore.js
+++ src/application/modules/administration/reconcile/stores/ReconcileViewStore.js
@@ -139,6 +139,11 @@ class ReconcileViewStore extends BaseListViewStore {
                                     <small style={{ display: "block" }}>
                                         {grant.address}
                                         </small>
+                                        {item.paymentType.abrv === 'ach' && (
+                                            <small style={{ display: "block" }}>
+                                            {grant.bankAccount}
+                                            </small>
+                                        )}
                                 </div>
                             } else {
                                 return <div>
@@ -156,6 +161,17 @@ class ReconcileViewStore extends BaseListViewStore {
                         type: 'function',
                         value: (item) => {
                             return item.isCashed ? 'Payment Received' : 'Payment Submited'
+                        }
+                    }
+                },
+                {
+                    key: 'isWithdraw',
+                    title: 'RECONCILE.LIST.COLUMNS.IS_WITHDRAW',
+                    format : {
+                        type: 'function',
+                        value: (item) => {
+                            return item.grants[0].isWithdraw ? <div className="type--center" ><i class="u-icon u-icon--approve u-icon--base "></i></div> : null;
+
                         }
                     }
                 },
