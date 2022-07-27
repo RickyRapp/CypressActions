210b8fe8f012de6c443479a0deda71c9d27def5a
MONO\arajher <arajher@mono-software.com>
2022-07-27 09:34:59 +0200

    fix responsive loader card in Dashboard Your Funds

--- src/themes/default/application/donor/dashboard/components/content-loader/YourFundsCardLoaderTemplate.jsx
+++ src/themes/default/application/donor/dashboard/components/content-loader/YourFundsCardLoaderTemplate.jsx
@@ -36,11 +36,11 @@ const YourFundsCardLoaderTemplate = props => {
 					<React.Fragment>
 						<rect x="0" y={12} rx="4" ry="4" width={'140'} height="22" />
 
-						<rect x="32%" y={115} rx="4" ry="4" width={'300'} height="45" />
-						<rect x="39%" y={175} rx="4" ry="4" width={'190'} height="12" />
+						<rect x={(loaderWidth / 2) - 150} y={115} rx="4" ry="4" width={'300'} height="45" />
+						<rect x={(loaderWidth / 2) - 95} y={175} rx="4" ry="4" width={'190'} height="12" />
 
-						<rect x="37%" y={240} rx="4" ry="4" width={'220'} height="35" />
-						<rect x="39%" y={300} rx="4" ry="4" width={'190'} height="12" />
+						<rect x={(loaderWidth / 2) - 110} y={240} rx="4" ry="4" width={'220'} height="35" />
+						<rect x={(loaderWidth / 2) - 95} y={300} rx="4" ry="4" width={'190'} height="12" />
 
 						<rect x="0%" y={'86%'} rx="4" ry="4" width={'46%'} height="60" />
 						<rect x="54%" y={'86%'} rx="4" ry="4" width={'46%'} height="60" />
@@ -49,11 +49,11 @@ const YourFundsCardLoaderTemplate = props => {
 					<React.Fragment>
 						<rect x="0" y={6} rx="4" ry="4" width={'140'} height="22" />
 
-						<rect x="32%" y={80} rx="4" ry="4" width={'300'} height="45" />
-						<rect x="39%" y={150} rx="4" ry="4" width={'190'} height="12" />
+						<rect x={(loaderWidth / 2) - 150} y={80} rx="4" ry="4" width={'300'} height="45" />
+						<rect x={(loaderWidth / 2) - 95} y={150} rx="4" ry="4" width={'190'} height="12" />
 
-						<rect x="37%" y={240} rx="4" ry="4" width={'220'} height="35" />
-						<rect x="39%" y={300} rx="4" ry="4" width={'190'} height="12" />
+						<rect x={(loaderWidth / 2) - 110} y={240} rx="4" ry="4" width={'220'} height="35" />
+						<rect x={(loaderWidth / 2) - 95} y={300} rx="4" ry="4" width={'190'} height="12" />
 
 						<rect x="0%" y={'86%'} rx="4" ry="4" width={'46%'} height="60" />
 						<rect x="54%" y={'86%'} rx="4" ry="4" width={'46%'} height="60" />
--- src/themes/default/application/donor/donor-donor/components/content-loader/AvailableBalanceLoaderTemplate.jsx
+++ src/themes/default/application/donor/donor-donor/components/content-loader/AvailableBalanceLoaderTemplate.jsx
@@ -25,8 +25,8 @@ const AvailableBalanceLoaderTemplate = props => {
 				<rect x="0" y={6} rx="4" ry="4" width={'250'} height="22" />
                 <rect x="0" y={50} rx="4" ry="4" width={loaderWidth > 400 ? '390' : '320'} height="14" />
                 
-				<rect x="28%" y={115} rx="4" ry="4" width={'315'} height="50" />
-				<rect x="36.5%" y={185} rx="4" ry="4" width={'200'} height="14" />
+				<rect x={(loaderWidth / 2) - 157} y={115} rx="4" ry="4" width={'315'} height="50" />
+				<rect x={(loaderWidth / 2) - 100} y={185} rx="4" ry="4" width={'200'} height="14" />
 
 			</ContentLoader>
 		</div>
