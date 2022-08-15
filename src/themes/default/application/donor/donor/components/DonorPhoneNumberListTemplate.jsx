aa620722d8a85750885a69664d43e859f74a2219
Alen Rajher <arajher@mono.software>
2022-08-15 11:13:27 +0200

    changed format number in profile settings

--- src/themes/default/application/donor/donor/components/DonorPhoneNumberListTemplate.jsx
+++ src/themes/default/application/donor/donor/components/DonorPhoneNumberListTemplate.jsx
@@ -37,17 +37,19 @@ const DonorAddressListTemplate = function({ donorPhoneNumberViewStore, t }) {
 							isAssignableAsPrimary={false}
 						/>
 					) : (
-						<div
-							className="row"
-							title="Click to edit"
-							onClick={() => onEnableEditClick(primaryPhoneNumber)}
-						>
+						<div className="row" title="Click to edit" onClick={() => onEnableEditClick(primaryPhoneNumber)}>
 							{primaryPhoneNumber && (
 								<div className="col col-sml-12 col-lrg-9" title="Click to edit" onClick={onEnableEditClick}>
 									<div className="row">
 										<div className="col col-sml-6 col-xxlrg-4">
 											<p className="type--sml type--wgt--regular type--color--opaque u-mar--bottom--sml">Number:</p>
-											<p className="type--base type--wgt--bold">{primaryPhoneNumber.number}</p>
+											<p className="type--base type--wgt--bold">
+												<FormatterResolver
+													item={primaryPhoneNumber}
+													field="number"
+													format={{ type: 'phone-number' }}
+												/>
+											</p>
 										</div>
 
 										<div className="col col-sml-6 col-xxlrg-4">
