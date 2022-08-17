1c4a8e15bce2ea29dbc5ba1108a0f51f786e03a6
Alen Rajher <arajher@mono.software>
2022-08-17 14:27:25 +0200

    added progress line loader in grants details

--- src/themes/default/application/donor/grant/pages/GrantPreviewTemplate.jsx
+++ src/themes/default/application/donor/grant/pages/GrantPreviewTemplate.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
 import { ApplicationEmptyState, FormatterResolver, Date, BaasicButton } from 'core/components';
-import { addressFormatter } from 'core/utils';
+import { addressFormatter, isSome } from 'core/utils';
 import { PreviewLayout } from 'core/layouts';
 import _ from 'lodash';
-import { PreviewCardLoaderTemplate } from 'themes/components';
+import { PreviewCardLoaderTemplate, ProgressTimelineLoaderTemplate } from 'themes/components';
+import { GrantProgressTimeline } from 'application/administration/grant/components';
 
 function GrantPreviewTemplate({ grantPreviewViewStore, t }) {
-	const { item, loaderStore, isEditable, cancelGrant, newGrant, editGrant, isCancelable } = grantPreviewViewStore;
-    
+	const {
+		item,
+		loaderStore,
+		isEditable,
+		cancelGrant,
+		newGrant,
+		editGrant,
+		isCancelable,
+		statusList,
+	} = grantPreviewViewStore;
+
 	return (
 		<PreviewLayout
 			store={grantPreviewViewStore}
@@ -17,135 +27,142 @@ function GrantPreviewTemplate({ grantPreviewViewStore, t }) {
 			loading={false}
 			layoutFooterVisible={false}
 		>
-			<div className="row">
-				<div className="col col-sml-12 col-lrg-8 col-xxlrg-6 col-xxxlrg-5">
-					<div className="card--primary card--med u-mar--bottom--med">
-						{loaderStore.loading ? (
-							<PreviewCardLoaderTemplate />
-						) : (
-							<div className="row">
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.CHARITY_LABEL')}
-									</div>
-									<span className="input--preview">
-										{item && (
-											<FormatterResolver
-												item={{ charity: item.charity }}
-												field="charity"
-												format={{ type: 'charity', value: 'charity-name-display' }}
-											/>
-										)}
-									</span>
+			<div className="container--base">
+				{loaderStore.loading ? (
+					<ProgressTimelineLoaderTemplate />
+				) : (
+					item &&
+					!(item.donationStatus.abrv === 'processed' && !isSome(item.debitCharityTransaction)) && ( //old grants
+						<GrantProgressTimeline item={item} statusList={statusList} />
+					)
+				)}
+
+				<div className="container--sml card--primary card--med u-mar--bottom--med">
+					{loaderStore.loading ? (
+						<PreviewCardLoaderTemplate />
+					) : (
+						<div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.CHARITY_LABEL')}
 								</div>
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.DATE_CREATED_LABEL')}
-									</div>
-									<span className="input--preview">{item && <Date format="full" value={item.dateCreated} />}</span>
-								</div>
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.AMOUNT_LABEL')}
-									</div>
-									<span className="input--preview">
-										{item && (
-											<FormatterResolver item={{ amount: item.amount }} field="amount" format={{ type: 'currency' }} />
-										)}
-									</span>
-								</div>
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.CONFIRMATION_NUMBER_LABEL')}
-									</div>
-									<span className="input--preview">
-										{item && <React.Fragment>{item.confirmationNumber}</React.Fragment>}
-									</span>
-								</div>
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.PURPOSE_TYPE_LABEL')}
-									</div>
-									<span className="input--preview">
-										{item && (
-											<React.Fragment>
-												{item.grantPurposeType.name} {`${item.purposeNote ? ' - ' + item.purposeNote : ''}`}
-											</React.Fragment>
-										)}
-									</span>
-								</div>
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.ACKNOWLEDGMENT_LABEL')}
-									</div>
-									<span className="input--preview">
-										{item && (
-											<React.Fragment>
-												{item.grantAcknowledgmentType.abrv === 'name-and-address' &&
-													`${item.donor.donorName} - ${addressFormatter.format(
-														_.find(item.donor.donorAddresses, { isPrimary: true }),
-														'full'
-													)}`}
-												{item.grantAcknowledgmentType.abrv === 'fund-name-and-address' &&
-													`${item.donor.fundName} - ${addressFormatter.format(
-														_.find(item.donor.donorAddresses, { isPrimary: true }),
-														'full'
-													)}`}
-												{item.grantAcknowledgmentType.abrv === 'fund-name' && item.donor.fundName}
-												{item.grantAcknowledgmentType.abrv === 'remain-anonymous' && 'Anonymous'}
-											</React.Fragment>
-										)}
-									</span>
-								</div>
-								{item && item.thirdPartyWebsite && (
-									<div className="col col-sml-12 u-mar--bottom--med">
-										<div className="type--base type--wgt--medium type--color--note">
-											{t('GRANT.PREVIEW.FIELDS.CHARITY_WEBSITE_LABEL')}
-										</div>
-										<span className="input--preview">
-											{item && (
-												<React.Fragment>
-													{item.thirdPartyWebsite.name} - {item.thirdPartyWebsite.url}
-												</React.Fragment>
-											)}
-										</span>
-									</div>
-								)}
-								{item && item.grantScheduledPayment && (
-									<div className="col col-sml-12 u-mar--bottom--med">
-										<div className="type--base type--wgt--medium type--color--note">
-											{t('GRANT.PREVIEW.FIELDS.SCHEDULED_LABEL')}
-										</div>
-										<span className="input--preview">
-											{item && <Date format="full" value={item.grantScheduledPayment.dateCreated} />}
-										</span>
-									</div>
-								)}
-								<div className="col col-sml-12 u-mar--bottom--med">
-									<div className="type--base type--wgt--medium type--color--note">
-										{t('GRANT.PREVIEW.FIELDS.ADDRESS')}
-									</div>
-									<span className="input--preview">
-										{item && (
-											<React.Fragment>{`${item.addressLine1}, ${item.addressLine2 ? item.addressLine2 + ', ' : ''} ${
-												item.city
-											}, ${item.state}, ${item.zipCode}`}</React.Fragment>
-										)}
-									</span>
-								</div>
-								{item && item.checkDeclinationReason && (
-									<div className="col col-sml-12 u-mar--bottom--sml">
-										<div className="type--base type--wgt--medium type--color--note">
-											{t('GRANT.PREVIEW.FIELDS.DECLINATION_REASON')}
-										</div>
-										<span className="input--preview">
-											{item && <React.Fragment>{`${item.checkDeclinationReason}`}</React.Fragment>}
-										</span>
-									</div>
-								)}
+								<span className="input--preview">
+									{item && (
+										<FormatterResolver
+											item={{ charity: item.charity }}
+											field="charity"
+											format={{ type: 'charity', value: 'charity-name-display' }}
+										/>
+									)}
+								</span>
 							</div>
-						)}
-					</div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.DATE_CREATED_LABEL')}
+								</div>
+								<span className="input--preview">{item && <Date format="full" value={item.dateCreated} />}</span>
+							</div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.AMOUNT_LABEL')}
+								</div>
+								<span className="input--preview">
+									{item && (
+										<FormatterResolver item={{ amount: item.amount }} field="amount" format={{ type: 'currency' }} />
+									)}
+								</span>
+							</div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.CONFIRMATION_NUMBER_LABEL')}
+								</div>
+								<span className="input--preview">
+									{item && <React.Fragment>{item.confirmationNumber}</React.Fragment>}
+								</span>
+							</div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.PURPOSE_TYPE_LABEL')}
+								</div>
+								<span className="input--preview">
+									{item && (
+										<React.Fragment>
+											{item.grantPurposeType.name} {`${item.purposeNote ? ' - ' + item.purposeNote : ''}`}
+										</React.Fragment>
+									)}
+								</span>
+							</div>
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.ACKNOWLEDGMENT_LABEL')}
+								</div>
+								<span className="input--preview">
+									{item && (
+										<React.Fragment>
+											{item.grantAcknowledgmentType.abrv === 'name-and-address' &&
+												`${item.donor.donorName} - ${addressFormatter.format(
+													_.find(item.donor.donorAddresses, { isPrimary: true }),
+													'full'
+												)}`}
+											{item.grantAcknowledgmentType.abrv === 'fund-name-and-address' &&
+												`${item.donor.fundName} - ${addressFormatter.format(
+													_.find(item.donor.donorAddresses, { isPrimary: true }),
+													'full'
+												)}`}
+											{item.grantAcknowledgmentType.abrv === 'fund-name' && item.donor.fundName}
+											{item.grantAcknowledgmentType.abrv === 'remain-anonymous' && 'Anonymous'}
+										</React.Fragment>
+									)}
+								</span>
+							</div>
+							{item && item.thirdPartyWebsite && (
+								<div className="u-mar--bottom--med">
+									<div className="type--base type--wgt--medium type--color--note">
+										{t('GRANT.PREVIEW.FIELDS.CHARITY_WEBSITE_LABEL')}
+									</div>
+									<span className="input--preview">
+										{item && (
+											<React.Fragment>
+												{item.thirdPartyWebsite.name} - {item.thirdPartyWebsite.url}
+											</React.Fragment>
+										)}
+									</span>
+								</div>
+							)}
+							{item && item.grantScheduledPayment && (
+								<div className="u-mar--bottom--med">
+									<div className="type--base type--wgt--medium type--color--note">
+										{t('GRANT.PREVIEW.FIELDS.SCHEDULED_LABEL')}
+									</div>
+									<span className="input--preview">
+										{item && <Date format="full" value={item.grantScheduledPayment.dateCreated} />}
+									</span>
+								</div>
+							)}
+							<div className="u-mar--bottom--med">
+								<div className="type--base type--wgt--medium type--color--note">
+									{t('GRANT.PREVIEW.FIELDS.ADDRESS')}
+								</div>
+								<span className="input--preview">
+									{item && (
+										<React.Fragment>{`${item.addressLine1}, ${item.addressLine2 ? item.addressLine2 + ', ' : ''} ${
+											item.city
+										}, ${item.state}, ${item.zipCode}`}</React.Fragment>
+									)}
+								</span>
+							</div>
+							{item && item.checkDeclinationReason && (
+								<div className="u-mar--bottom--sml">
+									<div className="type--base type--wgt--medium type--color--note">
+										{t('GRANT.PREVIEW.FIELDS.DECLINATION_REASON')}
+									</div>
+									<span className="input--preview">
+										{item && <React.Fragment>{`${item.checkDeclinationReason}`}</React.Fragment>}
+									</span>
+								</div>
+							)}
+						</div>
+					)}
 				</div>
 			</div>
 
