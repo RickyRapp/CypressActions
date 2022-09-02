73630ee01ad0e2af5b23d6041605add8faae82d8
Alen Rajher <arajher@mono.software>
2022-09-02 16:01:22 +0200

    #94739 - Remote Deposit - styled step 2

--- src/themes/default/application/charity/remote-deposit/components/CharityRemoteDepositInfoTemplate.jsx
+++ src/themes/default/application/charity/remote-deposit/components/CharityRemoteDepositInfoTemplate.jsx
@@ -5,95 +5,92 @@ import { CardItem } from 'themes/components';
 
 const CharityRemoteDepositInfoTemplate = ({ sessionCertificates, charity, t }) => {
     return (
-        <div>
-             {/* <CardItem label={"Charity"} /> */}
-            <div className="card--primary card--med type--base type--wgt--regular u-mar--bottom--sml">
-                <div className="card--tny card--secondary card--inline u-mar--bottom--sml">
-                    <span className="type--base type--wgt--medium type--color--opaque">
-                        
-                    </span>
-                    <span className="type--base type--wgt--bold u-push w--400--px">
-                        {charity.label}
-                    </span>
-                </div>
+        <div className="card--primary card--med">
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Charity</label>
+                <p className="type--wgt--bold">{charity.label}</p>
             </div>
 
-            <div className="card--primary card--med type--base type--wgt--regular u-mar--bottom--sml">
-                <div className="card--tny card--secondary u-mar--bottom--sml">
-                    <span className="type--base type--wgt--medium type--color--opaque">
-                        Checks scanned
-                    </span>
-                    <span className="type--base type--wgt--bold u-push">
-                        {`${sessionCertificates.length}`}
-                    </span>
-                </div>
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Checks scanned</label>
+                <p className="type--wgt--bold">{sessionCertificates.length}</p>
+            </div>
 
-                <div className="card--tny card--secondary u-mar--bottom--sml">
-                    <span className="type--base type--wgt--medium type--color--opaque">
-                        {t('GRANT.PREVIEW.FIELDS.AMOUNT_LABEL')}
-                    </span>
-                    <span className="type--base type--wgt--bold u-push">
-                        {sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">{t('GRANT.PREVIEW.FIELDS.AMOUNT_LABEL')}</label>
+                <p className="type--wgt--bold">
+                    {sessionCertificates.length > 0 ?
+                        sessionCertificates.map(c => c.insufficientFunds) &&
+                        <FormatterResolver
                             item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
                             field='amount'
                             format={{ type: 'currency' }}
-                        />}
-                    </span>
-                </div>
+                        />
+                        :
+                        "-"
+                    }
+                </p>
+            </div>
 
-                <div className="card--tny card--secondary u-mar--bottom--sml">
-                    <span className="type--base type--wgt--medium type--color--opaque">
-                        Fees
-                    </span>
-                    <span className="type--base type--wgt--bold u-push">
-                        {sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Fees</label>
+                <p className="type--wgt--bold">
+                    {sessionCertificates.length > 0 &&
+                        sessionCertificates.map(c => c.insufficientFunds) ?
+                        <FormatterResolver
                             item={{ amount: (sessionCertificates.map(c => c.denominationTypeValue).reduce((a, b) => a + b) - sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b)) }}
                             field='amount'
                             format={{ type: 'currency' }}
-                        />}
-                    </span>
-                </div>
+                        />
+                        :
+                        "-"
+                    }
+                </p>
+            </div>
 
-                <div className="card--tny card--secondary u-mar--bottom--sml">
-                    <span className="type--base type--wgt--medium type--color--opaque">
-                        Total (before fees)
-                    </span>
-                    <span className="type--base type--wgt--bold u-push">
-                        {sessionCertificates.length > 0 && <FormatterResolver
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Total (before fees)</label>
+                <p className="type--wgt--bold">
+                    {sessionCertificates.length > 0 ?
+                        <FormatterResolver
                             item={{ amount: sessionCertificates.map(c => c.denominationTypeValue).reduce((a, b) => a + b) }}
                             field='amount'
                             format={{ type: 'currency' }}
-                        />}
-                    </span>
-                </div>
+                        />
+                        :
+                        "-"
+                    }
+                </p>
+            </div>
 
-                <div className="card--primary card--med type--base type--wgt--regular u-mar--bottom--sml">
-                    <div className="card--tny card--secondary u-mar--bottom--sml">
-                        <span className="type--base type--wgt--medium type--color--opaque">
-                            Insufficient checks
-                        </span>
-                        <span className="type--base type--wgt--bold u-push">
-                            {sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) && <FormatterResolver
-                                item={{ amount: sessionCertificates.filter(c => c.insufficientFunds).map(c => c.certificateValue).reduce((a, b) => a + b, 0) }}
-                                field='amount'
-                                format={{ type: 'currency' }}
-                            />}
-                        </span>
-                    </div>
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Insufficient checks</label>
+                <p className="type--wgt--bold">
+                    {sessionCertificates.length > 0 && sessionCertificates.map(c => c.insufficientFunds) ?
+                        <FormatterResolver
+                            item={{ amount: sessionCertificates.filter(c => c.insufficientFunds).map(c => c.certificateValue).reduce((a, b) => a + b, 0) }}
+                            field='amount'
+                            format={{ type: 'currency' }}
+                        />
+                        :
+                        "-"
+                    }
+                </p>
+            </div>
 
-                    <div className="card--tny card--secondary u-mar--bottom--sml">
-                        <span className="type--base type--wgt--medium type--color--opaque">
-                            <span className=" type--color--note">Grand total</span> (including insufficient checks)
-                        </span>
-                        <span className="type--base type--wgt--bold u-push">
-                            {sessionCertificates.length > 0 && <FormatterResolver
-                                item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
-                                field='amount'
-                                format={{ type: 'currency' }}
-                            />}
-                        </span>
-                    </div>
-                </div>
+            <div className="u-mar--bottom--sml">
+                <label className="form__group__label">Grand total (including insufficient checks)</label>
+                <p className="type--wgt--bold">
+                    {sessionCertificates.length > 0 ?
+                        <FormatterResolver
+                            item={{ amount: sessionCertificates.map(c => c.certificateValue).reduce((a, b) => a + b) }}
+                            field='amount'
+                            format={{ type: 'currency' }}
+                        />
+                        :
+                        "-"
+                    }
+                </p>
             </div>
         </div>
     )
--- src/themes/default/application/charity/remote-deposit/components/Step3Template.jsx
+++ src/themes/default/application/charity/remote-deposit/components/Step3Template.jsx
@@ -3,18 +3,12 @@ import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
 import { BaasicFormControls, BaasicButton, BaasicModal, FormatterResolver } from 'core/components';
 import { BlankCertificateModal } from '.';
-import {CharityRemoteDepositInfoTemplate} from './'
+import { CharityRemoteDepositInfoTemplate } from './'
 class Step3Template extends React.Component {
 	constructor(props) {
 		super(props);
 	}
-	// componentDidMount() {
-	// 	document.addEventListener('keydown', this.props.onBarcodeChange, true);
-	// }
 
-	// componentWillUnmount() {
-	// 	document.removeEventListener('keydown', this.props.onBarcodeChange, true);
-	// }
 	render() {
 		const {
 			form,
@@ -30,95 +24,104 @@ class Step3Template extends React.Component {
 			charity
 		} = this.props;
 		return (
-			<React.Fragment>
-				<div className="card--primary card--med u-mar--bottom--sml">
-					<h3 className=" u-mar--bottom--med">General Data</h3>
-					<div className="u-mar--bottom--lrg">
-						<div className="row scanner__table--head">
-							<div className="col col-lrg-1 type--base type--wgt--medium"></div>
-							<div className="col col-lrg-2 type--base type--wgt--medium">{t('Certificate Number')}</div>
-							<div className="col col-lrg-3 type--base type--wgt--medium">{t('Barcode')}</div>
-							<div className="col col-lrg-2 type--base type--wgt--medium">{t('Denomination')}</div>
-							<div className="col col-lrg-2 type--base type--wgt--medium">{t('Amount')}</div>
-							<div className="col col-lrg-2 type--base type--wgt--medium">{t('Action')}</div>
+			<div className="container--sidebar">
+				<div>
+					<div className="card--primary card--med u-mar--bottom--sml">
+						{/* <h3 className=" u-mar--bottom--med">General Data</h3> */}
+						<div className="u-mar--bottom--med">
+							<div className="row scanner__table--head">
+								<div className="col col-lrg-1 type--base type--wgt--medium"></div>
+								<div className="col col-lrg-2 type--base type--wgt--medium">{t('Certificate Number')}</div>
+								<div className="col col-lrg-3 type--base type--wgt--medium">{t('Barcode')}</div>
+								<div className="col col-lrg-2 type--base type--wgt--medium type--right">{t('Denomination')}</div>
+								<div className="col col-lrg-2 type--base type--wgt--medium type--right">{t('Amount')}</div>
+								<div className="col col-lrg-2 type--base type--wgt--medium type--center">{t('Action')}</div>
+							</div>
+							{sessionCertificates.map((c, index) => {
+								return (
+									<div className="row scanner__table--body" key={c.barcode}>
+										<div className="col col-lrg-1 type--sml type--wgt--medium">
+											{index + 1}.
+										</div>
+										<div className="col col-lrg-2 type--sml type--wgt--medium">
+											{c.bookletCode}-{c.certificateCode}
+										</div>
+										<div className="col col-lrg-3 type--sml type--wgt--medium">{c.barcode}</div>
+										<div className="col col-lrg-2 type--sml type--wgt--medium type--right">{`$${c.denominationTypeValue}`}</div>
+										<div className="col col-lrg-2 type--sml type--wgt--medium type--right">
+											${c.certificateValue} {c.insufficientFunds ? ` - insufficient funds` : ''}
+										</div>
+										<div className="col col-lrg-2 type--sml type--wgt--medium type--center">
+											{c.isBlank ? <BaasicButton
+												className="btn btn--icon"
+												onlyIconClassName="u-mar--right--tny"
+												icon="u-icon u-icon--edit u-icon--sml"
+												label="CONTRIBUTION.LIST.BUTTON.EDIT"
+												onlyIcon={true}
+												onClick={() => editCheck(c)}
+											></BaasicButton> : null}
+											<BaasicButton
+												className="btn btn--icon"
+												onlyIconClassName="u-mar--right--tny"
+												icon="u-icon u-icon--close--secondary u-icon--sml"
+												label="CONTRIBUTION.LIST.BUTTON.CANCEL"
+												onlyIcon={true}
+												onClick={() => cancelCertificate(c.barcode)}
+											></BaasicButton>
+										</div>
+									</div>
+								);
+							})}
 						</div>
-						{sessionCertificates.map((c, index) => {
-							return (
-								<div className="row scanner__table--body" key={c.barcode}>
-									<div className="col col-lrg-1 type--sml type--wgt--medium">
-										{index + 1}.
-									</div>
-									<div className="col col-lrg-2 type--sml type--wgt--medium">
-										{c.bookletCode}-{c.certificateCode}
-									</div>
-									<div className="col col-lrg-3 type--sml type--wgt--medium">{c.barcode}</div>
-									<div className="col col-lrg-2 type--sml type--wgt--medium">{`$${c.denominationTypeValue}`}</div>
-									<div className="col col-lrg-2 type--sml type--wgt--medium">
-										${c.certificateValue} {c.insufficientFunds ? ` - insufficient funds` : ''}
-									</div>
-									<div className="col col-lrg-2 type--sml type--wgt--medium">
-										{c.isBlank ? <BaasicButton
-											className="btn btn--icon"
-											onlyIconClassName="u-mar--right--tny"
-											icon="u-icon u-icon--edit u-icon--base"
-											label="CONTRIBUTION.LIST.BUTTON.EDIT"
-											onlyIcon={true}
-											onClick={() => editCheck(c)}
-										></BaasicButton> : null}
-										<BaasicButton
-											className="btn btn--icon"
-											onlyIconClassName="u-mar--right--tny"
-											icon="u-icon u-icon--close--secondary u-icon--base"
-											label="CONTRIBUTION.LIST.BUTTON.CANCEL"
-											onlyIcon={true}
-											onClick={() => cancelCertificate(c.barcode)}
-										></BaasicButton>
-									</div>
-								</div>
-							);
-						})}
-					</div>
 
-					<div className="row">
-						<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
-							<input
-								type="text"
-								className="input input--lrg input--text"
-								id="barcode"
-								value={barcode}
-								onChange={onBarcodeChange}
-								maxLength={10}
-								onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+						<div className="row">
+							<div className="col col-sml-12 col-lrg-4 u-mar--bottom--sml">
+								<input
+									type="text"
+									className="input input--lrg input--text"
+									id="barcode"
+									value={barcode}
+									onChange={onBarcodeChange}
+									maxLength={10}
+									onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+								/>
+
+							</div>
+						</div>
+						<BaasicModal modalParams={blankCertificateModal} showClose={false}>
+							<BlankCertificateModal />
+						</BaasicModal>
+
+						<div className="u-separator--primary u-mar--bottom--med u-mar--top--sml"></div>
+
+						<div className="u-display--flex flex--primary">
+							<BaasicButton
+								className="btn btn--med btn--med--wide btn--primary u-mar--right--sml"
+								onClick={removeFromCache}
+								label="Cancel Session"
 							/>
 
+							<div>
+								<BaasicButton
+									className="btn btn--med btn--med--wide btn--primary u-mar--right--sml"
+									onClick={onPreviousStepClick}
+									label="SESSION.CREATE.STEP2.BUTTONS.BACK"
+								/>
+								<BaasicFormControls
+									form={form}
+									onSubmit={form.onSubmit}
+									// disableSave={sessionCertificates.length === 0}
+									label="SESSION.CREATE.STEP2.BUTTONS.SAVE"
+								/>
+							</div>
 						</div>
 					</div>
-					<BaasicModal modalParams={blankCertificateModal} showClose={false}>
-						<BlankCertificateModal />
-					</BaasicModal>
-
-					<div>
-						<BaasicButton
-							className="btn btn--med btn--med--wide btn--primary u-mar--right--sml"
-							onClick={onPreviousStepClick}
-							label="SESSION.CREATE.STEP2.BUTTONS.BACK"
-						/>
-						<BaasicButton
-							className="btn btn--med btn--med--wide btn--primary u-mar--right--sml"
-							onClick={removeFromCache}
-							label="Cancel Session"
-						/>
-					</div>
-					<BaasicFormControls
-						form={form}
-						onSubmit={form.onSubmit}
-						// disableSave={sessionCertificates.length === 0}
-						label="SESSION.CREATE.STEP2.BUTTONS.SAVE"
-					/>
 				</div>
 
-				<CharityRemoteDepositInfoTemplate sessionCertificates={sessionCertificates} charity={charity} t={t} />
-			</React.Fragment >
+				<div>
+					<CharityRemoteDepositInfoTemplate sessionCertificates={sessionCertificates} charity={charity} t={t} />
+				</div>
+			</div >
 		);
 	}
 }
