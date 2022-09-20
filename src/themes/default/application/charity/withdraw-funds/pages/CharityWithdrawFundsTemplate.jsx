1dc332636503f335705fc63c1318ce0bc54bf02f
Alen Rajher <arajher@mono.software>
2022-09-20 15:35:47 +0200

    styled withdraw funds layout

--- src/themes/default/application/charity/withdraw-funds/pages/CharityWithdrawFundsTemplate.jsx
+++ src/themes/default/application/charity/withdraw-funds/pages/CharityWithdrawFundsTemplate.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { defaultTemplate } from 'core/hoc';
@@ -17,146 +18,82 @@ function CharityWithdrawFundsTemplate({ charityWithdrawFundsViewStore, t }) {
         bankAccountValidationMessage,
         addressValidationMessage
     } = charityWithdrawFundsViewStore;
+    const hasBankAccount = bankAccountDropdownStore.length >= 1 || bankAccountDropdownStore.originalItems.length >= 1;
+
+    const handleAchTab = (e) => {
+        e.target.checked = true;
+        onChange(e.target.checked);
+    }
+
+    const handleEmailTab = (e) => {
+        e.target.checked = false;
+        onChange(e.target.checked);
+    }
 
     return (
         <Page>
-            <div className="card--primary card--med withdraw__container">
-                <p className="withdraw__title">Available Balance</p>
-                <p className="withdraw__price">
-                    <FormatterResolver
-                        item={{ balance: availableBalance }}
-                        field="balance"
-                        format={{ type: 'currency' }}
-                    />
-                </p>
-                <p className="type--lrg">How much would you like to withdraw?</p>
-               {/* <p className="type--sml type--color--opaque u-mar--bottom--sml">Minimum $500 investment required.</p> */}
-
-                <div className="withdraw__form">
-                    <div className="u-mar--top--sml">
-                        <BaasicInput
-                            id="withdrawAmount"
-                            className="input input--lrg"
-                            placeholder="GRANT.LIST.FILTER.DOLLAR_RANGE_PLACEHOLDER"
-                            onChange={e => {
-                                changeValue(e.target.value);
-                            }}
-                        />
-                        <p class="validation__message">{amountValidationMessage}</p>
-                    </div>
-                    <div className="u-mar--top--sml">
-                        <div className="input--switch u-clearfix">
-                            <BaasicInput
-                                type="radio"
-                                id={"isACH"}
-                                name={"isACH"}
-                                checked={isACH === true}
-                                onChange={e => {
-                                    e.target.checked = true;
-                                    onChange(e.target.checked);
-                                }}
-                                label={`ACH`}
-                                wrapperClassName="input--switch__wrapper"
-                            />
-                            <BaasicInput
-                                type="radio"
-                                id={"isCheck"}
-                                name={"isCheck"}
-                                checked={isACH === false}
-                                onChange={e => {
-                                    e.target.checked = false;
-                                    onChange(e.target.checked);
-                                }}
-                                label={`Check by mail`}
-                                wrapperClassName="input--switch__wrapper"
-                            />
-                        </div>
-                        {isACH ?
-                            <div className="u-mar--top--sml">
-                                {bankAccountDropdownStore.length >= 1 || bankAccountDropdownStore.originalItems.length >= 1 ?
-                                    <div className="form__group">
-                                        <BaasicDropdown
-                                            store={bankAccountDropdownStore}
-                                            placeholder="BANK_ACCOUNT.LIST.EMPTY_STATE.SELECT"
-                                        // onChange={selectBankAccount}
-                                        />
-                                        <p class="validation__message">{bankAccountValidationMessage}</p>
-                                    </div>
-                                    : <p>{t("BANK_ACCOUNT.LIST.EMPTY_STATE.TITLE")}</p>}
-                            </div>
-                            :
-                            <div className="u-mar--top--sml">
-                                <div className="row row--form">
-                                    <p class="validation__message">{addressValidationMessage}</p>
-                                    <div className="form__group col col-sml-12 col-lrg-13">
-                                        <p>{t('ADDRESS.EDIT.FIELDS.ADDRESS_LINE_1_LABEL')}</p>
-                                        <BaasicInput
-                                            id="addressLine1"
-                                            className="input input--lrg"
-                                            placeholder="ADDRESS.EDIT.FIELDS.ADDRESS_LINE_1_PLACEHOLDER"
-                                            value={charityAddress.addressLine1}
-                                            disabled={true}
-                                            onChange={event => (charityAddress.addressLine1 = event.target.value)}
-                                        />
-                                    </div>
-                                    <div className="form__group col col-sml-12 col-lrg-13">
-                                        <p>{t('ADDRESS.EDIT.FIELDS.ADDRESS_LINE_2_LABEL')}</p>
-                                        <BaasicInput
-                                            id="addressLine2"
-                                            className="input input--lrg"
-                                            placeholder="ADDRESS.EDIT.FIELDS.ADDRESS_LINE_2_PLACEHOLDER"
-                                            value={charityAddress.addressLine2}
-                                            disabled={true}
-                                            onChange={event => (charityAddress.addressLine2 = event.target.value)}
-                                        />
-                                    </div>
-                                    <div className="form__group col col-sml-12 col-lrg-12">
-                                        <p>{t('ADDRESS.EDIT.FIELDS.CITY_LABEL')}</p>
-                                        <BaasicInput
-                                            id="city"
-                                            className="input input--lrg"
-                                            placeholder="ADDRESS.EDIT.FIELDS.CITY_PLACEHOLDER"
-                                            value={charityAddress.city}
-                                            disabled={true}
-                                            onChange={event => (charityAddress.city = event.target.value)}
-                                        />
-                                    </div>
-                                    <div className="form__group col col-sml-12 col-lrg-12">
-                                        <p>{t('ADDRESS.EDIT.FIELDS.STATE_LABEL')}</p>
-                                        <BaasicInput
-                                            id="state"
-                                            className="input input--lrg"
-                                            placeholder="ADDRESS.EDIT.FIELDS.STATE_PLACEHOLDER"
-                                            value={charityAddress.state}
-                                            disabled={true}
-                                            onChange={event => (charityAddress.state = event.target.value)}
-                                        />
-                                    </div>
-                                    <div className="form__group col col-sml-12 col-lrg-12">
-                                        <p>{t('ADDRESS.EDIT.FIELDS.ZIPCODE_LABEL')}</p>
-                                        <BaasicInput
-                                            id="zipCode"
-                                            className="input input--lrg"
-                                            placeholder="ADDRESS.EDIT.FIELDS.ZIPCODE_PLACEHOLDER"
-                                            value={charityAddress.zipCode}
-                                            disabled={true}
-                                            onChange={event => (charityAddress.zipCode = event.target.value)}
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        }
-                    </div>
-                </div>
-                <div>
-                    <BaasicButton
-                        className="btn btn--med btn--primary"
-                        label={'LIST_LAYOUT.WITHDRAW_FUNDS'}
-                        onClick={createWithdraw}
-                    />
+            <div className="container--base container--sidebar">
+                <div className="c-deposit__list c-deposit__list--2">
+                    <CardTab action={handleAchTab} title={"ACH"} isActive={isACH} icon={"ach"} />
+                    <CardTab action={handleEmailTab} title={"Check by mail"} isActive={!isACH} icon={"check"} />
                 </div>
             </div>
-        </Page>
+
+            <div className="container--lrg container--sidebar">
+                <div className="card--primary card--med u-order--1--from--xlrg">
+                    {hasBankAccount || !isACH && <h3 className="u-mar--bottom--med">How much would you like to withdraw?</h3>}
+                    <div>
+                        {hasBankAccount || !isACH &&
+                            <div className="u-mar--top--sml">
+                                <BaasicInput
+                                    id="withdrawAmount"
+                                    className="input input--lrg"
+                                    placeholder="GRANT.LIST.FILTER.DOLLAR_RANGE_PLACEHOLDER"
+                                    onChange={e => {
+                                        changeValue(e.target.value);
+                                    }}
+                                />
+                                <p className="validation__message">{amountValidationMessage}</p>
+                            </div>
+                        }
+                        <div>
+                            {isACH ?
+                                <React.Fragment>
+                                    {hasBankAccount ?
+                                        <div className="u-mar--top--sml">
+                                            <div className="form__group">
+                                                <BaasicDropdown
+                                                    store={bankAccountDropdownStore}
+                                                    placeholder="BANK_ACCOUNT.LIST.EMPTY_STATE.SELECT"
+                                                />
+                                                <p className="validation__message">{bankAccountValidationMessage}</p>
+                                            </div>
+                                        </div>
+                                        :
+                                        <EmptyState t={t} />
+                                    }
+                                </React.Fragment>
+                                :
+                                <EmailForm charityAddress={charityAddress} t={t} addressValidationMessage={addressValidationMessage} />
+                            }
+                        </div>
+                    </div>
+                    {!isACH &&
+                        <div>
+                            <BaasicButton
+                                className="btn btn--med btn--primary"
+                                label={'LIST_LAYOUT.WITHDRAW_FUNDS'}
+                                onClick={createWithdraw}
+                            />
+                        </div>
+                    }
+                </div>
+
+                <div className="u-order--1 u-order--2--from--lrg">
+                    <AvailableBalance availableBalance={availableBalance} />
+                </div>
+            </div >
+        </Page >
     );
 }
 
@@ -164,5 +101,113 @@ CharityWithdrawFundsTemplate.propTypes = {
     charityWithdrawFundsViewStore: PropTypes.object.isRequired,
     t: PropTypes.func,
 };
+
+const EmptyState = ({ t }) => {
+    return (
+        <div className="emptystate--primary">
+            <i className="u-icon u-icon--lrg u-icon--bank--grey u-mar--bottom--tny"></i>
+            <p className="emptystate--primary__title u-mar--bottom--med">{t("BANK_ACCOUNT.LIST.EMPTY_STATE.TITLE")}</p>
+
+            <button className="btn btn--med btn--med--wide btn--secondary " title={'Click to insert'}>
+                Connect your bank
+            </button>
+        </div>
+    )
+}
+
+const CardTab = ({ isActive, action, title, icon }) => {
+    return (
+        <div className={`c-deposit__card ${isActive ? "active" : ""}`} onClick={action}>
+            <div className="flex--grow--1">
+                <i className={`u-icon u-icon--med u-icon--${icon} ${isActive ? "active" : ""}`}></i>
+            </div>
+            <div className="flex--grow--2">
+                <h5 className={`type--base type--wgt--medium ${isActive ? "type--color--negative" : ""}`}>
+                    {title}
+                </h5>
+            </div>
+        </div>
+    )
+}
+
+const AvailableBalance = ({ availableBalance }) => {
+    return (
+        <div className="card--primary card--med ">
+            <p className="dashboard-card__body--title">Available Balance</p>
+            <p className="dashboard-card__body--amount">
+                <FormatterResolver
+                    item={{ balance: availableBalance }}
+                    field="balance"
+                    format={{ type: 'currency' }}
+                />
+            </p>
+        </div>
+    )
+}
+
+const EmailForm = ({ charityAddress, t, addressValidationMessage }) => {
+    return (
+        <div className="u-mar--top--sml">
+            <div className="row row--form">
+                <p className="validation__message">{addressValidationMessage}</p>
+                <div className="form__group col col-sml-12 col-lrg-13">
+                    <p>{t('ADDRESS.EDIT.FIELDS.ADDRESS_LINE_1_LABEL')}</p>
+                    <BaasicInput
+                        id="addressLine1"
+                        className="input input--lrg"
+                        placeholder="ADDRESS.EDIT.FIELDS.ADDRESS_LINE_1_PLACEHOLDER"
+                        value={charityAddress.addressLine1}
+                        disabled={true}
+                        onChange={event => (charityAddress.addressLine1 = event.target.value)}
+                    />
+                </div>
+                <div className="form__group col col-sml-12 col-lrg-13">
+                    <p>{t('ADDRESS.EDIT.FIELDS.ADDRESS_LINE_2_LABEL')}</p>
+                    <BaasicInput
+                        id="addressLine2"
+                        className="input input--lrg"
+                        placeholder="ADDRESS.EDIT.FIELDS.ADDRESS_LINE_2_PLACEHOLDER"
+                        value={charityAddress.addressLine2}
+                        disabled={true}
+                        onChange={event => (charityAddress.addressLine2 = event.target.value)}
+                    />
+                </div>
+                <div className="form__group col col-sml-12 col-lrg-12">
+                    <p>{t('ADDRESS.EDIT.FIELDS.CITY_LABEL')}</p>
+                    <BaasicInput
+                        id="city"
+                        className="input input--lrg"
+                        placeholder="ADDRESS.EDIT.FIELDS.CITY_PLACEHOLDER"
+                        value={charityAddress.city}
+                        disabled={true}
+                        onChange={event => (charityAddress.city = event.target.value)}
+                    />
+                </div>
+                <div className="form__group col col-sml-12 col-lrg-12">
+                    <p>{t('ADDRESS.EDIT.FIELDS.STATE_LABEL')}</p>
+                    <BaasicInput
+                        id="state"
+                        className="input input--lrg"
+                        placeholder="ADDRESS.EDIT.FIELDS.STATE_PLACEHOLDER"
+                        value={charityAddress.state}
+                        disabled={true}
+                        onChange={event => (charityAddress.state = event.target.value)}
+                    />
+                </div>
+                <div className="form__group col col-sml-12 col-lrg-12">
+                    <p>{t('ADDRESS.EDIT.FIELDS.ZIPCODE_LABEL')}</p>
+                    <BaasicInput
+                        id="zipCode"
+                        className="input input--lrg"
+                        placeholder="ADDRESS.EDIT.FIELDS.ZIPCODE_PLACEHOLDER"
+                        value={charityAddress.zipCode}
+                        disabled={true}
+                        onChange={event => (charityAddress.zipCode = event.target.value)}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default defaultTemplate(CharityWithdrawFundsTemplate);
